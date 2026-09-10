5a5e57d9ed8d64905de0fac90fae740ca6546353
HussainAhmed <hussainahmed1203010139@gmail.com>
2026-09-10 20:26:18 +0600

    Create variable7.c

---
+++ variable7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+  int studentID = 59;
+  int studentAge = 23;
+  float cGpa = 3.59;
+  char studentGrade = 'B';
+
+  // Print variables
+  printf("Student id: %d\n", studentID);
+  printf("Student age: %d\n", studentAge);
+  printf("Student CGPA: %f\n", cGpa);
+  printf("Student grade: %c", studentGrade);
+
+  return 0;
+}
