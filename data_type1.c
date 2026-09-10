ded925b0589fa8a109e615ceee0e3b7df8939c9c
HussainAhmed <hussainahmed1203010139@gmail.com>
2026-09-10 20:31:27 +0600

    Create data_type1.c

---
+++ data_type1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  
+  int myNum = 59;               
+  float goal = 3.75;     
+  char grade = 'A';        
+  
+  
+  printf("%d\n", myNum);
+  printf("%f\n", goal);
+  printf("%c\n", grade);
+  return 0;
+}
