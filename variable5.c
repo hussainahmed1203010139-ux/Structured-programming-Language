698dedd2355c1356565f807b22abfdfc7baebc5f
HussainAhmed <hussainahmed1203010139@gmail.com>
2026-09-10 20:20:44 +0600

    Create variable5.c

---
+++ variable5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int x = 5;
+  int y = 6;
+  int sum = x + y;
+  int multiplication= x*y;
+  printf("Sum = %d\n", sum);
+  printf("Multiplication = %d\n", multiplication);
+  return 0;
+}
