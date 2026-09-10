300aa480ae08967343366e44a5ef2a67f510cb73
HussainAhmed <hussainahmed1203010139@gmail.com>
2026-09-10 21:42:09 +0600

    Create data_type5.c

---
+++ data_type5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  float myFloatNum = 3.5;
+
+  printf("%f\n", myFloatNum); 
+  printf("%.1f\n", myFloatNum); 
+  printf("%.2f\n", myFloatNum); 
+  printf("%.4f", myFloatNum);  
+  return 0;
+}
